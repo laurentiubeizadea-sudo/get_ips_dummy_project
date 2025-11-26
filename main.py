5e48835496cb273b64084d424d7ee3f8b1b3af85
laurentiubeizadea <laurentiu.beizadea@gmail.com>
2025-11-26 12:45:32 +0200

    done refactoring

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-"""main module"""
+"""main module for the git session"""
 
-def get_name_and_age() -> str:
-	"""dummy function"""
 
-	return "Dan is 30 years old"
+def get_ip_addresses() -> list:
+    """dummy function"""
+
+    return ["10.111.122.68", "10.111.122.69"]
+
+
+if __name__ == "__main__":
+    ips = get_ip_addresses()
+    print(ips)
